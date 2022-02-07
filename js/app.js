073f409c79dfd2b50e55eeb0b0f54626a0f63647
Dieter Vrancken <dieter.v91@gmail.com>
2022-02-07 21:42:07 +0100

    tabs toegevoegd

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // FEATURES MOUSE OVER == DIETER
     $('.feature').hover(
-        function() {
-            $(this).children('.info').stop().show().fadeTo( "slow" , 1);
-            $(this).children('h2').stop().hide().fadeTo( "slow" , 0).hide();
+        function () {
+            $(this).children('.info').stop().show().fadeTo("slow", 1);
+            $(this).children('h2').stop().hide().fadeTo("slow", 0).hide();
         },
-        function() {
-            $(this).children('.info').stop().fadeTo( "slow" , 0).hide();
-            $(this).children('h2').stop().show().fadeTo( "slow" , 1);
+        function () {
+            $(this).children('.info').stop().fadeTo("slow", 0).hide();
+            $(this).children('h2').stop().show().fadeTo("slow", 1);
         }
     )
+    $('.tabs').cardTabs({ 'theme': 'themakleur' });
 });
