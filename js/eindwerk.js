1bf3102bfe00f84278b4416b9432b8c4cafb28b7
Dieter Vrancken <dieter.v91@gmail.com>
2022-02-10 10:52:06 +0100

    added socials + sources files

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -47,8 +47,13 @@ $(document).ready(function () {
     $(window).on('scroll', function () {
         if ($(window).scrollTop() > 100) {
             backToTop.css('display', 'flex');
+
         } else {
             backToTop.hide();
         }
     })
+    //socials == DIETER
+    $('#share-bar').share({
+        theme: 'square'
+    });
 });
