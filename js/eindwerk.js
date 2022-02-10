d98fb55f3f579737688423c722bca2f1c14f79d5
Yens <yens.b@hotmail.be>
2022-02-10 10:40:01 +0100

    changes added jquery yens

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -21,34 +21,42 @@ $(document).ready(function () {
 });
 //einde code Ciro//
 
-$(document).ready(function () {
-    // FEATURES MOUSE OVER == DIETER
-    $('.feature').hover(
-        function () {
-            $(this).children('.info').stop().show().fadeTo("slow", 1);
-            $(this).children('h2').stop().hide().fadeTo("slow", 0).hide();
-        },
-        function () {
-            $(this).children('.info').stop().fadeTo("slow", 0).hide();
-            $(this).children('h2').stop().show().fadeTo("slow", 1);
-        }
-    )
+// FEATURES MOUSE OVER == DIETER
+$('.feature').hover(
+    function () {
+        $(this).children('.info').stop().show().fadeTo("slow", 1);
+        $(this).children('h2').stop().hide().fadeTo("slow", 0).hide();
+    },
+    function () {
+        $(this).children('.info').stop().fadeTo("slow", 0).hide();
+        $(this).children('h2').stop().show().fadeTo("slow", 1);
+    }
+)
 
-    $('.tabs').cardTabs({ 'theme': 'themakleur' });
+$('.tabs').cardTabs({ 'theme': 'themakleur' });
 
-    //back-to-top == DIETER
+//back-to-top == DIETER
 
-    const backToTop = $('#back-to-top')
-    backToTop.on('click', function () {
-        $('html,body').animate({
-            scrollTop: 0
-        }, 500);
-    })
-    $(window).on('scroll', function () {
-        if ($(window).scrollTop() > 100) {
-            backToTop.css('display', 'flex');
-        } else {
-            backToTop.hide();
-        }
-    })
+const backToTop = $('#back-to-top')
+backToTop.on('click', function () {
+    $('html,body').animate({
+        scrollTop: 0
+    }, 500);
+})
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() > 100) {
+        backToTop.css('display', 'flex');
+    } else {
+        backToTop.hide();
+    }
+    //Begin code Yens //
+    $('#succes').hide();
+    $(function () {
+
+        // shows succes message
+        $('#myForm').ajaxForm(function () {
+            $('#succes').show();
+        });
+    });
+    //Einde code Yens //
 });
